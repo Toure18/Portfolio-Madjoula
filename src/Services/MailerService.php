0e6d6629abc45b481b7c2bffcca67022fcdc10d7
Toure Madjoula <tmadjoula@gmail.com>
2021-04-12 23:06:01 +0200

    create service mail

---
+++ src/Services/MailerService.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class MailerService
+{
+    /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * MailerService constructor.
+     *
+     * @param MailerInterface $mailer
+     * @param Environment $twig
+     */
+    public function __construct(MailerInterface $mailer, Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param string $subject
+     * @param string $from
+     * @param string $to
+     * @param string $template
+     * @param array $parameters
+     * @throws TransportExceptionInterface
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function send(string $subject, string $from, string $replyTo, string $to, string $template, array $parameters): void
+    {
+        try {
+            $email = (new Email())
+                ->from($from)
+                ->to($to)
+                ->replyTo($replyTo)
+                ->subject($subject)
+                ->html(
+                    $this->twig->render($template, $parameters)
+                );
+
+            $this->mailer->send($email);
+        } catch (TransportException $e) {
+            print $e->getMessage()."\n";
+            throw $e;
+        }
+
+    }
+}
